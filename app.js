321c335296b67eeccbbe63c1c359c344f7d7cd6f
Gordon Rawe <gordon.tongji@hotmail.com>
2015-10-24 12:10:33 +0800

    something

--- app.js
+++ app.js
@@ -12,12 +12,13 @@ app.set('views', __dirname + '/views');
 app.set('view cache', false);
 // To disable Swig's cache, do the following:
 swig.setDefaults({ cache: false });
+app.use(require('express').static(__dirname + '/static-resources'));
 // NOTE: You should always cache templates in a production environment.
 // Don't leave both of these to `false` in production!
 
 app.get('/', function (req, res) {
-  res.render('index', {title:"Welcome here...",content:"this is the content for this page..."});
+  res.render('index', {title:"Welcome here...",content:"this is the content for this page...",active:"contact"});
 });
 
-app.listen(1337);
-console.log('Application Started on http://localhost:1337/');
+app.listen(18080);
+console.log('Application Started on http://localhost:18080/');
